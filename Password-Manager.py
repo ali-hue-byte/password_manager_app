378afa84a039ed68cde893f1947ef94f7fe93058
ali-hue-byte <elguelouiali@gmail.com>
2025-12-23 10:13:20 +0100

    Update Password-Manager.py

--- Password-Manager.py
+++ Password-Manager.py
@@ -306,7 +306,7 @@ def start(fernet):
 
 
 main_label = ctk.CTkLabel(app, text="Welcome to Password Manager", font=("Arial", 30, "bold"))
-main_label.pack(pady=100, anchor="center")
+main_label.pack(pady=70, anchor="center")
 
 MASTER_FILE = "master_password"
 SALT_FILE = "salt"
@@ -323,8 +323,7 @@ main_Entry = ctk.CTkEntry(app, placeholder_text=msg, show="*")
 main_Entry.pack(pady=30, anchor="center")
 main_button = ctk.CTkButton(app, text="Start application", command=check)
 main_button.pack( padx=10, pady=30, anchor ="center")
-exit_btn = ctk.CTkButton(app, text="Exit", command=exit)
-exit_btn.pack(padx=10, pady=10, anchor="center")
+
 
 
 
